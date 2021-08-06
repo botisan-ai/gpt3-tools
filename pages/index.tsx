b998243b3b761ea84a0388cf26f7f05c60862a8f
Simon Liang <no.lhr0909@gmail.com>
2021-08-06 22:04:28 +0800

    update branding

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <AppLayout>
       <h2 style={{ padding: '5px' }}>Hello!</h2>
-      <p style={{ padding: '5px' }}>This is a UI Starter for NextJS using Ant Design and styled-components.</p>
+      <p style={{ padding: '5px' }}>This is a UI Tool for GPT-3.</p>
     </AppLayout>
   );
 }
